30851984503942ac41ab41ac67271cd2ab1a3742
Lucas Levi <lucas.lsl12@gmail.com>
2020-08-17 21:48:45 -0300

    selecting more than one file

--- app/public/src/controller/DropBoxController.js
+++ app/public/src/controller/DropBoxController.js
@@ -19,11 +19,53 @@ class DropBoxController {
 
         this.inputFilesEl.addEventListener('change', event=>{
 
-            console.log(event.target.files);
+            this.uploadTask(event.target.files);
 
             this.snackModalEl.style.display ='block';
 
         });
 
     }
+
+    uploadTask(files){
+
+        let promises = []; // subir um arquivo ou subir vários
+
+        [...files].forEach(file=>{
+
+            promises.push(new Promise((resolve, reject)=>{
+
+                let ajax = new XMLHttpRequest();
+
+                ajax.open('POST','/upload')
+
+                ajax.onload = event => { //invocando o ajax assim que carregar 
+
+                    try {
+                        resolve(JSON.parse(ajax.responseText));
+                    } catch (e){
+                        reject(e);
+                    }
+
+                };
+                ajax.onerror = event =>{ //Caso erro ao fazer upload
+
+                    reject(event);
+
+                };
+
+                let formData = new FormData ();
+
+                formData.append('input-file', file); // append é para juntar
+
+                ajax.send(formData); // usando a API form data para enviar arquivo
+
+            }));
+
+        });
+
+        return Promise.all(promises);// Array de promessas
+
+    }
+
 }
