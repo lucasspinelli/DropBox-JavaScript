23e7d582ac4e4c94951904e04119a25aa3e34dc0
Lucas Levi <lucas.lsl12@gmail.com>
2020-08-28 22:16:05 -0300

    remove file

--- app/public/src/controller/DropBoxController.js
+++ app/public/src/controller/DropBoxController.js
@@ -55,12 +55,28 @@ class DropBoxController {
             let file = JSON.parse(li.dataset.file);
             let key = li.dataset.key;
 
-            let formData = new FormData();
+           
+            promises.push(new Promise((resolve, reject)=>{
 
-            formData.append('path', file.path);
-            formData.append('key', key);
+                let fileRef = firebase.storage().ref(this.currentFolder.join('/')).child(file.name);
 
-            promises.push(this.ajax('/file','DELETE', formData));
+                fileRef.delete().then(()=>{
+
+                    resolve({
+
+                        fields:{
+                            key
+                        }
+
+                    });
+
+                }).catch(err=>{
+
+                    reject(err);
+    
+                });
+
+            }));
 
         });
 
