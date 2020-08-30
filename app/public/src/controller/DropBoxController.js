82f8c042df123e4588ea7d7efc259a7c2a6a0e30
Lucas Levi <lucas.lsl12@gmail.com>
2020-08-30 19:03:49 -0300

    open file in data base project Over

--- app/public/src/controller/DropBoxController.js
+++ app/public/src/controller/DropBoxController.js
@@ -721,7 +721,7 @@ class DropBoxController {
                     break;
 
                 default:
-                    window.open('/file?path='+file.path);
+                    window.open(file.path);
                     break;
 
             }
